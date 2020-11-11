4a7b1f60f95213157f999807b1e1e003a49cfe8c
César Filho <cesarfilho09.cf@gmail.com>
2020-11-10 21:08:56 -0300

    Seção inicial e Carousel

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import Header from './components/Header/Header';
 import './index.css';
+import Slider from "react-slick";
+
 function App() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
   return (
+    
     <>
       <Header/>
+      <section className="section-hero">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <Slider {...settings}>
+                  <div >
+                    a
+                  </div>
+                  <div>
+                    a
+                  </div>
+              </Slider>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
