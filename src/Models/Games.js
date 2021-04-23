1cb41d1b510a808cfd9273259cfebd4e78bba4fd
Cesar Filho <cesar.filho@vizir.com.br>
2021-04-23 18:42:06 -0300

    banner hero

--- src/Models/Games.js
+++ src/Models/Games.js
@@ -6,25 +6,10 @@ export function listGamesHeader(){
           "value" : "299.00"
         },
         {
-          "image" : "http://demo.gloriathemes.com/wp/cloux/wp-content/uploads/2017/12/nfs-slider.jpg",
-          "title" : "Grand Theft Auto V",
-          "value" : "99.00"
-        },
-        {
-          "image" : "http://demo.gloriathemes.com/wp/cloux/wp-content/uploads/2017/12/egypt-slider-1920x941.jpg",
+          "image" : "http://demo.gloriathemes.com/wp/cloux/wp-content/uploads/2017/12/fs-7-1920x950.jpg",
           "title" : "Cyberpunk 2077",
           "value" : "299.00"
         },
-        // {
-        //   "image" : "/images/games/pes.jpg",
-        //   "title" : "eFootball PES 2021 - Season Update 2020/2021",
-        //   "value" : "199.00"
-        // },
-        // {
-        //   "image" : "/images/games/witcher.jpg",
-        //   "title":"The Witcher 3 : Wild Hunt - Game of the Year Edition",
-        //   "value" : "87.99"
-        // },
       ]
       
 }
--- src/components/ItemCarouselDestaque/ItemCarouselDestaque.jsx
+++ src/components/ItemCarouselDestaque/ItemCarouselDestaque.jsx
@@ -4,8 +4,30 @@ import "./ItemCarouselDestaque.css";
 function ItemCarouselDestaque(props) {
   return (
     <>
-      <div className="container-image">
+      <div className="container-image d-flex justify-content-center">
         <img src={props.image} alt="" className="img-fluid" />
+        <div className="container-text">
+            <h4 className="display-2 text-center font-weight-bold title-game">
+              Nome do Game
+            </h4>
+            <p className="lead text-white text-center font-weight-bold">
+                Optio asperiores error eum, provident sequi aliquam iure nisi reiciendis quod temporibus 
+                <br/>
+                suscipit deleniti perspiciatis iusto facilis nulla eos porro esse praesentium!
+            </p>
+            <div className="d-flex justify-content-center">
+                <div>
+                    <button className="btn btn-hero ">
+                        Ver Mais
+                    </button>
+                </div>
+                <div className="ml-4">
+                    <button className="btn btn-hero ">
+                        Comprar R$ 120,00
+                    </button>
+                </div>
+            </div>
+        </div>
       </div>
     </>
   );
